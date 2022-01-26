3c697a718cde581a41f188ba32e1f9685299ad79
Matthew Brooks <matt@mattbrooks.ca>
2022-01-25 21:53:02 -0500

    Adjust netrw

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -27,8 +27,8 @@ keymap("n", "<leader><leader>", ":edit #<cr>", opts)
 
 -- Quick
 -- keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
-keymap("n", "<leader>e", ":Lexplore %:h<cr>", opts)
-keymap("n", "<leader>E", ":Lexplore .<cr>", opts)
+keymap("n", "<leader>e", ":30Lexplore %:h<cr>", opts)
+keymap("n", "<leader>E", ":30Lexplore .<cr>", opts)
 keymap("n", "<leader>w", ":w<cr>", opts)
 keymap("n", "<leader>q", ":q<cr>", opts)
 keymap("n", "<leader>Q", ":%bd|e#<cr>", opts)
